5fde92e97f041a016af62a6f91a257b88f86e080
Swastik-1234 <swastikpanda69@gmail.com>
2025-03-02 03:55:06 +0530

    Updated database configuration

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql');
-require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
+  database: process.env.DB_NAME || 'railway',
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false
   }
-  console.log('Successfully connected to database');
 });
 
-module.exports = connection; 
+module.exports = connection;
